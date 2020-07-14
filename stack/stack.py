fb31070cc26ccffb09f55b6bcf2ec0b4c7d438ec
zack-murray <61975667+zack-murray@users.noreply.github.com>
2020-07-13 20:53:18 -0400

    1st assignment stack complete, need to submit retro

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,6 @@
+  
+from singly_linked_list import LinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +13,51 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-class Stack:
+# Part 1: Implement stack class using an array
+class ArrayStack:
+    # Initialize class with size of 0 and empty stack/list
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-
+        self.storage = []
+    
+    # Check length of self.storage stack/list
     def __len__(self):
-        pass
+        return len(self.storage)
 
+    # Append value to the top of the stack/list
     def push(self, value):
-        pass
+        self.storage.append(value)
 
+    # Deletes value from top of the stack/list, returns the value at the top of the stack
     def pop(self):
-        pass
+        # For pop and peek methods, check whether stack is empty, to avoid exceptions
+        if len(self.storage) > 0:
+            return self.storage.pop()
+        else:
+            return None
+
+# Part 2: Re-implement stack class using a linked list
+class Stack():
+    # Initialize class with size of 0 and empty stack/list
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    # Check length of self.storage stack/list
+    def __len__(self):
+        return self.size
+
+    # Append value to the end of the stack/list
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    # Deletes value from end of the stack/list, returns the element at the top of the stack
+    def pop(self):
+        # If stack is empty, return nothing
+        if self.size == 0:
+            return None
+        # If stack has values, remove the last one
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
